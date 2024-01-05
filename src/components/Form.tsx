a79ff44722ba17de41e34c1f0822adb20166f225
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2024-01-05 13:05:16 -0300

    added style to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,24 +5,39 @@ const Form = () => {
         <label className="text-sm" htmlFor="name">
           Nome
         </label>
-        <input type="text" placeholder="Digite o seu nome" />
+        <input
+          type="text"
+          placeholder="Digite o seu nome"
+          className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
+        />
       </div>
       <div className="flex flex-col">
         <label className="text-sm" htmlFor="email">
           E-mail
         </label>
-        <input type="email" placeholder="Digite o seu e-mail" />
+        <input
+          type="email"
+          placeholder="Digite o seu e-mail"
+          className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"
+        />
       </div>
       <div className="flex flex-col">
-        <a href="#">Ler os termos</a>
-        <div>
+        <a href="#" className="text-xs underline mb-2 ">
+          Ler os termos
+        </a>
+        <div className="flex gap-2 items-center">
           <input type="checkbox" />
           <label className="text-sm" htmlFor="agree">
             Concordo com os termos
           </label>
         </div>
       </div>
-      <button>Cadastrar</button>
+      <button
+        type="submit"
+        className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white"
+      >
+        Cadastrar
+      </button>
     </form>
   );
 };
